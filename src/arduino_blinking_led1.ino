3cd08cd0bc59e94a916e806a794838bea5a374a0
johnnytesttt <jandavidvl@gmail.com>
2026-08-12 19:33:11 -0400

    Update arduino_blinking_led1.ino

--- src/arduino_blinking_led1.ino
+++ src/arduino_blinking_led1.ino
@@ -8,7 +8,7 @@ void setup()
 void loop()
 {
   digitalWrite(12,HIGH);
-  delay(1000); // Wait for 1000 millisecond(s)
+  delay(1000); // Wait for 1000 milliseconds (1 second)
   digitalWrite(12, LOW);
-  delay(1000); // Wait for 1000 millisecond(s)
+  delay(1000); // Wait for 1000 milliseconds (1 second)
 }
